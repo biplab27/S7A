e5b0f57326d03df78093dba12847aef86a248465
Biplab Sinha <biplab@quantseye.ai>
2025-01-09 09:58:44 +0530

    New model with DepthwiseSeparableConv, is promissing, giving 99.26% with 6K params.

--- train_test_model6.py
+++ train_test_model6.py
@@ -38,55 +38,66 @@ class DepthwiseSeparableConv(nn.Module):
         return x
 
 class Net(nn.Module):
-    def __init__(self, dropout_rate=0.1):
+    def __init__(self, dropout_value=0.1):
         super(Net, self).__init__()
-        
-        # Input Block: 1 → 8
+        # Input Block
         self.convblock1 = nn.Sequential(
-            DepthwiseSeparableConv(in_channels=1, out_channels=8, kernel_size=3),
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(8),
-            nn.Dropout(dropout_rate)
-        )
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 26
 
         # CONVOLUTION BLOCK 1
         self.convblock2 = nn.Sequential(
-            DepthwiseSeparableConv(in_channels=8, out_channels=16, kernel_size=3),
+            DepthwiseSeparableConv(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=0),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout(dropout_rate)
-        )
-        self.convblock3 = nn.Sequential(
-            DepthwiseSeparableConv(in_channels=16, out_channels=24, kernel_size=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(24),
-            nn.Dropout(dropout_rate)
-        )
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 24
 
         # TRANSITION BLOCK 1
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=24, out_channels=8, kernel_size=1, bias=False),
-        )
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 24
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12
 
         # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            DepthwiseSeparableConv(in_channels=10, out_channels=32, kernel_size=(3, 3), padding=0),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 10
         self.convblock5 = nn.Sequential(
-            DepthwiseSeparableConv(in_channels=8, out_channels=16, kernel_size=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout(dropout_rate)
-        )
+            DepthwiseSeparableConv(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 8
         self.convblock6 = nn.Sequential(
-            DepthwiseSeparableConv(in_channels=16, out_channels=24, kernel_size=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(24),
-            nn.Dropout(dropout_rate)
-        )
-
-        # OUTPUT BLOCK
-        self.gap = nn.AdaptiveAvgPool2d(1)
+            DepthwiseSeparableConv(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 6
         self.convblock7 = nn.Sequential(
-            nn.Conv2d(in_channels=24, out_channels=10, kernel_size=1, bias=False)
+            DepthwiseSeparableConv(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 6
+        
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=6)
+        ) # output_size = 1
+
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            # nn.BatchNorm2d(10),
+            # nn.ReLU(),
+            # nn.Dropout(dropout_value)
         )
 
     def forward(self, x):
@@ -97,8 +108,10 @@ class Net(nn.Module):
         x = self.convblock4(x)
         x = self.convblock5(x)
         x = self.convblock6(x)
-        x = self.gap(x)
         x = self.convblock7(x)
+        x = self.gap(x)        
+        x = self.convblock8(x)
+
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
 
@@ -185,6 +198,7 @@ if __name__ == "__main__":
 
     # Data loading
     train_transforms = transforms.Compose([
+        transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
